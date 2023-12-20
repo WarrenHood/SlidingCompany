7d10a4d41b72873c838c4ba09fe284716af64a05
Warren Hood <nullbyte001@gmail.com>
2023-12-20 06:16:12 +0200

    Initial implementation

---
+++ SlidingCompany/PlayerSlideController.cs
@@ -0,0 +1,116 @@
+﻿using GameNetcodeStuff;
+using UnityEngine;
+
+namespace SlidingCompany
+{
+    public class PlayerSlideController: MonoBehaviour {
+        public PlayerControllerB playerController = null;
+        public float slideSpeed = 0f;
+        public float initialSlideSpeedBoost = 15.0f;
+        public float slideFriction = 0.1f;
+        public float friction = 0.95f;
+        const float gravity = 30.0f;
+        const float initialSlideStaminaCost = 0.08f;
+        bool isSliding = false;
+        bool isCrouching = false;
+        PhysicMaterial originalMaterial = null;
+        PhysicMaterial slideMaterial = null;
+
+        void Start () {
+            originalMaterial = playerController.thisController.material;
+            slideMaterial = new PhysicMaterial("PlayerSlideMaterial");
+            slideMaterial.staticFriction = slideFriction;
+            slideMaterial.dynamicFriction = slideFriction;
+        }
+
+        void FixedUpdate() {
+            if (!isCrouching) {
+                // If we aren't already crouching
+                isCrouching = playerController.isCrouching;
+
+                // Ensure we are in our crouching animation, even after crouching mid-air
+                if (isCrouching) {
+                    playerController.playerBodyAnimator.SetBool("crouching", true);
+                }
+
+                // If we were already moving in some direction beforehand, we can start sliding if not exhausted
+                isSliding = !playerController.isExhausted && playerController.sprintMeter >= initialSlideStaminaCost && isCrouching && playerController.thisController.velocity.magnitude > 0;
+                if (isSliding && playerController.thisController.isGrounded) {
+                    // Carry over any existing velocity into the slide
+                    slideSpeed = playerController.thisController.velocity.magnitude + initialSlideSpeedBoost;
+
+                    // Let's also use a bit of stamina since we gave a small speed boost
+                    playerController.sprintMeter = Mathf.Clamp(playerController.sprintMeter - initialSlideStaminaCost, 0f, 1f);
+
+                    // TODO: Begin playing slide audio
+                }
+                else if (isSliding) {
+                    // If we weren't grounded, we can queue a slide for when we land by setting isCrouching to false
+                    isCrouching = false;
+                }
+            }
+            else {
+                // If we aren't crouching already
+                if (!playerController.isCrouching) {
+                    isCrouching = false;
+                    isSliding = false;
+                }
+            }
+
+
+            if (!isSliding && !isCrouching) {
+                // TODO: Stop playing sliding audio
+
+                if (playerController.thisController.material != originalMaterial) {
+                    // Reset the material to the original
+                    playerController.thisController.material = originalMaterial;
+                }
+                return;
+            }
+
+            RaycastHit hit;
+            Ray floorRay = new Ray(playerController.gameplayCamera.transform.position, Vector3.down);
+            Vector3 slideDirection = playerController.gameplayCamera.transform.forward;
+
+            // If we are crouching on a slope, start sliding
+            if (playerController.thisController.isGrounded) {
+                // Apply some friction
+                slideSpeed *= friction;
+
+                Physics.Raycast(floorRay, out hit, 20.0f, playerController.playersManager.allPlayersCollideWithMask, QueryTriggerInteraction.Ignore);
+                if (!isSliding) {
+                    // Carry over any existing velocity into the slide
+                    slideSpeed = playerController.thisController.velocity.magnitude;
+                    isSliding = true;
+                    // TODO: Begin playing slide audio
+                }
+                slideDirection = Vector3.ProjectOnPlane(slideDirection, hit.normal).normalized;
+
+                // Steepness of the slope
+                float steepness = -Vector3.Dot(slideDirection, Vector3.up);
+                slideSpeed += steepness * gravity * Time.fixedDeltaTime;
+                if (slideSpeed < 0.1) {
+                    slideSpeed = 0f;
+                    // TODO: Stop playing sliding audio
+                }
+            }
+            else {
+                slideSpeed = 0f;
+                isSliding = false;
+            }
+
+            if (isSliding) {
+                // Slide in some direction
+
+                playerController.thisController.material = slideMaterial;
+                playerController.thisController.Move(slideDirection * slideSpeed * Time.fixedDeltaTime);
+            }
+            else {
+                if (playerController.thisController.material != originalMaterial) {
+                    // Reset the material to the original
+                    playerController.thisController.material = originalMaterial;
+                }
+            }
+        }
+    }
+}
--- SlidingCompany/Plugin.cs
+++ SlidingCompany/Plugin.cs
@@ -1,5 +1,10 @@
 ﻿using BepInEx;
+using HarmonyLib;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using GameNetcodeStuff;
+using System.Reflection;
+using System.Collections;
 
 namespace SlidingCompany
 {
@@ -10,6 +15,92 @@ namespace SlidingCompany
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPatch(typeof(GameNetcodeStuff.PlayerControllerB), "Start")]
+        [HarmonyPostfix]
+        static void AttachPlayerSlideScript(GameNetcodeStuff.PlayerControllerB __instance) {
+            // Attaches the PlayerSlideController component to this gameobject
+            PlayerSlideController slideController = __instance.thisPlayerBody.gameObject.AddComponent<PlayerSlideController>();
+            slideController.playerController = __instance;
+        }
+
+        [HarmonyPatch(typeof(GameNetcodeStuff.PlayerControllerB), "Crouch_performed")]
+        [HarmonyPrefix]
+        static bool AllowCrouchWhileJumping(GameNetcodeStuff.PlayerControllerB __instance, InputAction.CallbackContext context) {
+            if (!context.performed) {
+                return false;
+            }
+            if (__instance.quickMenuManager.isMenuOpen) {
+                return false;
+            }
+            if ((!__instance.IsOwner || !__instance.isPlayerControlled || (__instance.IsServer && !__instance.isHostPlayerObject)) && !__instance.isTestingPlayer) {
+                return false;
+            }
+            if (__instance.inSpecialInteractAnimation || __instance.isTypingChat) {
+                return false;
+            }
+            __instance.Crouch(!__instance.isCrouching);
+
+            // Skip the original Crouch_performed
+            return false;
+        }
+
+        [HarmonyPatch(typeof(GameNetcodeStuff.PlayerControllerB), "Jump_performed")]
+        [HarmonyPrefix]
+        static bool AllowJumpWhileCrouching(
+            GameNetcodeStuff.PlayerControllerB __instance,
+            InputAction.CallbackContext context,
+            ref bool ___isJumping,
+            ref float ___playerSlidingTimer,
+            ref Coroutine ___jumpCoroutine
+        ) {
+            if (__instance.quickMenuManager.isMenuOpen) {
+                return false;
+            }
+            if ((!__instance.IsOwner || !__instance.isPlayerControlled || (__instance.IsServer && !__instance.isHostPlayerObject)) && !__instance.isTestingPlayer) {
+                return false;
+            }
+            if (__instance.inSpecialInteractAnimation) {
+                return false;
+            }
+            if (__instance.isTypingChat) {
+                return false;
+            }
+            if (__instance.isMovementHindered > 0 && !__instance.isUnderwater) {
+                return false;
+            }
+            if (__instance.isExhausted) {
+                return false;
+            }
+
+            // Check if player is near the ground
+            Ray interactRay = new Ray(__instance.transform.position, Vector3.down);
+            bool isNearGround = Physics.Raycast(interactRay, 0.15f, StartOfRound.Instance.allPlayersCollideWithMask, QueryTriggerInteraction.Ignore);
+
+            if ((__instance.thisController.isGrounded || (!___isJumping && isNearGround)) 
+                && !___isJumping 
+                && (!__instance.isPlayerSliding || ___playerSlidingTimer > 2.5f)) {
+                ___playerSlidingTimer = 0f;
+                ___isJumping = true;
+
+                // Uncrouch if we are crouching
+                if (__instance.isCrouching) {
+                    __instance.isCrouching = false;
+                    __instance.playerBodyAnimator.SetBool("crouching", false);
+                }
+                
+                __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter - 0.08f, 0f, 1f);
+                __instance.movementAudio.PlayOneShot(StartOfRound.Instance.playerJumpSFX);
+                if (___jumpCoroutine != null) {
+                    __instance.StopCoroutine(___jumpCoroutine);
+                }
+
+                MethodInfo playerJumpMethodInfo = typeof(PlayerControllerB).GetMethod("PlayerJump", BindingFlags.NonPublic | BindingFlags.Instance);
+                ___jumpCoroutine = __instance.StartCoroutine((IEnumerator)playerJumpMethodInfo.Invoke(__instance, new object[] { }));
+            }
+            return false;
         }
     }
 }
